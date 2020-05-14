104519c43ec6fa996b7cf66c6e4f42a03765ceb6
Roman Chumakov <chumakoff.r.v@gmail.com>
2020-05-15 01:49:14 +0500

    Adds API for obtaining film information from IMDb platform via POST query. Reduces requirements.txt. Adds dispensable changes to .gitignore.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from imdb import IMDb
 from flask import Flask, request, render_template
 from kinopoisk.movie import Movie
+import json
 
 app = Flask(__name__)
 
@@ -16,8 +17,23 @@ def index():
     print(matrix.keys())
     return render_template('index.html', title='', description='', year='', critic='')
 
+@app.route('/api/get-search-results/', methods=['post'])
+def api_imdb():
+    name = request.data.film
+    ia = IMDb()
+    results = ia.search_movie(name)
+    data = []
+
+    for result in results:
+        ia.update(result)
+        movie = {}
+        if 'title' in result.keys():movie['title'] = result['title']
+        if 'rating' in result.keys(): movie['rating'] = result['rating']
+        if 'votes' in result.keys(): movie['votes'] = result['votes']
+        if 'full-size cover url' in result.keys(): movie['image'] = result['full-size cover url']
+        data.append(movie)
+    return json.dumps(data) # str(results)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
